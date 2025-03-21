b45ddc8137c9493d69c02e42bd3feceaefc4b283
Neutrino2711 <akshajpal24@gmail.com>
2025-03-21 16:29:44 +0530

    initial styling

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
 import './UserItem.css'
+import Avatar from '../../shared/components/UIElements/Avatar';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Card from '../../shared/components/UIElements/Card'
 
 
 const UserItem = props => {
     return <li className='user-item'>
-        < div className='user-item__content'>
-            <div className='user-item__image'>
-                <img src={props.image} alt={props.name} />
+        <Card className='user-item__content'>
+            <Link to={`/${props.uid}/places`}>
+                <div className='user-item__image'>
+                    <Avatar image={props.image} alt={props.name} />
 
-            </div>
-            <div className='user-item__info'>
-                <h2>{props.name}</h2>
-                <h3>{props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}</h3>
-            </div>
-        </div>
+                </div>
+                <div className='user-item__info'>
+                    <h2>{props.name}</h2>
+                    <h3>{props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}</h3>
+                </div>
+            </Link>
+        </Card>
     </li>
 }
 
