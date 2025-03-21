cc628b83380a400ae400d7dd0db9ed81d5d480a6
Neutrino2711 <akshajpal24@gmail.com>
2025-03-21 15:51:50 +0530

    user-module-setup

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return <h1>User Works!</h1>
+
+    const USERS = [
+        { id: 'u1', image: 'https://gratisography.com/wp-content/uploads/2025/01/gratisography-dog-vacation-800x525.jpg', name: "Akshaj Pal", placesCount: 3 }
+    ]
+
+    return <UsersList items={USERS} />
 }
 
 export default Users;
