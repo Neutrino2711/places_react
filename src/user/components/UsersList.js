688b58028ed608dbf645d60da9768f6b43cea47c
Neutrino2711 <akshajpal24@gmail.com>
2025-03-24 15:46:17 +0530

    side-drawer-added

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css'
+
 
 const UsersList = props => {
     if (props.items.length === 0) {
         return (
             <div className="center">
-                <h2>No Users found.</h2>
+                <Card>
+                    <h2>No Users found.</h2>
+                </Card>
             </div>
         );
     }
 
     return (
-        <ul className='users-list'>
+        <ul>
             {props.items.map(user => (
                 <UserItem
                     key={user.id}
