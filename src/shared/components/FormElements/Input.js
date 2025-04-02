6dcc1ea8cd54ceec1d179801bc6b0da99be63eea
Neutrino2711 <akshajpal24@gmail.com>
2025-04-02 12:55:43 +0530

    form-submission-added

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import './Input.css';
 import { validate } from "../../util/validators";
@@ -25,6 +25,16 @@ const inputReducer = (state, action) => {
 const Input = props => {
 
     const [inputState, dispatch] = useReducer(inputReducer, { value: '', isValid: false, isTouched: false });
+
+    const { id, onInput } = props;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+        onInput(id, value, isValid);
+
+    },
+        [id, value, isValid, onInput]
+    );
 
     const changeHandler = event => {
         dispatch({ type: 'CHANGE', val: event.target.value, validators: props.validators });
